acbaae505888f0a018e2145167b792c1f8ad471a
João Felipe Silva Lamounier <jjoaolamounier@gmail.com>
2023-12-08 16:04:04 -0300

    Criado menu hamburguer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
+import {Router} from "@angular/router";
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  model!: MenuItem[];
 
+  constructor(private router: Router, private el: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    this.model = [
+      {
+        icon: 'pi pi-user',
+        label: 'Dados Pessoais'
+      },
+      {
+        icon: 'pi pi-users',
+        label: 'Gerenciar Adm.'
+      },
+      {
+        icon: 'pi pi-sign-out',
+        label: 'Sair'
+      }
+    ];
+  }
+
+  ngAfterViewInit(): void {
+    const splitButton = this.el.nativeElement.querySelector('.p-splitbutton-defaultbutton');
+    if (splitButton) {
+      splitButton.parentNode.removeChild(splitButton);
+    }
+  }
+  goToHome(): void {
+    this.router.navigate(['/home']);
+  }
 }
